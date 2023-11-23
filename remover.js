f9f4a534978d8f9bcd6b93db959ba6fbb3a8b272
Stefan <43815245+stefan9999991@users.noreply.github.com>
2023-11-24 00:47:10 +0100

    Update remover.js
    Fixed when reload with YouTube button shorts shelf came back

--- remover.js
+++ remover.js
@@ -1,19 +1,41 @@
-if (window.location.href.includes("youtube.com")) {
-    waitForElm('.style-scope.ytd-guide-section-renderer').then((elm) => {
-        console.log('Element is ready');
-        const elements = document.getElementsByClassName("title style-scope ytd-guide-entry-renderer");
-        for (let element_index = 0; element_index < elements.length; element_index++) {
-            if (elements[element_index].innerText.toLowerCase() == "shorts") {
-                console.log("Found Short button element");
-                console.log("Something " + elements[element_index].innerText);
-                elements[element_index].parentElement.parentElement.parentElement.innerHTML = "";
+function removeShorts() {
+    if (window.location.href.includes("youtube.com")) {
+        // Remove the shorts navigation bar on the right
+        waitForElm('.style-scope.ytd-guide-section-renderer').then((elm) => {
+            console.log('Element is ready');
+            const elements = document.getElementsByClassName("title style-scope ytd-guide-entry-renderer");
+            for (let element_index = 0; element_index < elements.length; element_index++) {
+                if (elements[element_index].innerText.toLowerCase() == "shorts") {
+                    console.log("Found Short button element");
+                    console.log("Something " + elements[element_index].innerText);
+                    elements[element_index].parentElement.parentElement.parentElement.innerHTML = "";
+                }
             }
-        }
-    });
+        });
+    
+        // Remove the shorts shelf on the page
+        waitForElm('span#title.style-scope.ytd-rich-shelf-renderer').then((elm) => {
+            console.log('Element is ready');
+            const elements = document.querySelectorAll("span#title.style-scope.ytd-rich-shelf-renderer");
+            // Check if the shorts shelf is already rendered
+            if (elements.length < 2) {
+                setTimeout(removeShelf, 100);
+            } else {
+                removeShelf();
+            }
+        });
+    }
+}
 
-    waitForElm('span#title.style-scope.ytd-rich-shelf-renderer').then((elm) => {
-        console.log('Element is ready');
-        const elements = document.querySelectorAll("span#title.style-scope.ytd-rich-shelf-renderer");
+function removeShelf() {
+    // Keep checking whether the shorts shelf has already rendered, if so remove it.
+    const elements = document.querySelectorAll("span#title.style-scope.ytd-rich-shelf-renderer");
+    console.log("Number of elements: " + elements.length);
+    if (elements.length == 0) {
+        return;
+    } else if (elements.length < 2) {
+        setTimeout(removeShelf, 100);
+    } else {
         for (let element_index = 0; element_index < elements.length; element_index++) {
             if (elements[element_index].innerText.toLowerCase() == "shorts") {
                 console.log("Found Short shelf element");
@@ -21,15 +43,16 @@ if (window.location.href.includes("youtube.com")) {
                 elements[element_index].parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.innerHTML = "";
             }
         }
-    });
+    }
 }
 
-window.onload = function() {
+function loaded() {
     // similar behavior as clicking on a link
     console.log("WHAAAAAAAAAA: " + window.location.href);
     if (window.location.href.includes("youtube.com/shorts")) {
         window.location.href = "https://www.youtube.com/";
     }
+    removeShorts();
 }
 
 function waitForElm(selector) {
@@ -51,3 +74,6 @@ function waitForElm(selector) {
         });
     });
 }
+
+window.onload = loaded;
+window.addEventListener('yt-navigate-start', loaded, true);
